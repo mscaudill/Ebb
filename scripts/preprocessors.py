d5869e267499d173f296fcfdcc47b71e82cd3738
mscaudill <mscaudill@gmail.com>
2023-11-04 15:07:39 -0500

    [dev]: add preprocessors to notch filter, downsample and resave files as well as to restrict channels for Spindle

--- scripts/preprocessors.py
+++ scripts/preprocessors.py
@@ -2,9 +2,13 @@
 
     standard:
         A preprocessor for notch filtering, downsampling and trimming EDF files.
-    spindlize:
+    spindle:
         A preprocessor for performing channel selection for upload to SPINDLE
         software site.
+
+    This module also includes a batch processing function that concurrently
+    calls a preprocessor on all files within a directory.
+
     batch:
         A function that concurrently runs a preprocessor on all the EDF files in
         a supplied directory.
@@ -12,6 +16,7 @@
 Each preprocessor write a new EDF file.
 """
 
+import copy
 import pickle
 import time
 import warnings
@@ -30,11 +35,12 @@ from openseize.resampling import resampling
 from ebb.core import concurrency
 from ebb.masking import masks
 
-def standard(path: Union[str, Path],
-    savedir: path: Union[str, Path],
+def standard(
+    path: Union[str, Path],
+    savedir: Union[str, Path],
     fs: float,
     downsample: int,
-    notch_width: float,
+    notch_width: float = 6,
     trim_to: Sequence = [48, 72],
     chunksize=30e5,
     axis=-1,
@@ -43,124 +49,137 @@ def standard(path: Union[str, Path],
     """A standard preprocessor that notch filters, downsamples and trims data.
 
     Args:
-        
-    """
-
-
-def preprocess(path, savedir, channels=[0, 1, 3], fs=5000, M=25, fstop=60,
-               fwidth=6, nominally=[48, 72], chunksize=30e5, axis=-1,
-               returning=False, verbose=False):
-    """Preprocesses and EDF file at path by constructing, notch filtering and
-    downsampling a producer.
-
-    Args:
-        epath:
-            Path to an edf file.
+        path:
+            Path to an edf file to process
         savedir:
-            Directory location where preprocessed EDF will be saved to. The
-            filename to save will be named epath.stem + '_preprocessed'. 
-        channels:
-            The channels to include in the processed producer.
+            Directory where processed EDF will be saved. The filename will match
+            the stem of path with the addition of '_PREPROCESSED' before the
+            extension.
         fs:
-            The sampling rate at which the data was collected.
-        M:
-            The downsample factor to reduce the number of samples produced by
-            the returned processed producer.
-        fstop:
-            The stop frequency of the preprocessing Notch IIR filter.
-        fwidth:
-            The width of the preprocessing Notch IIR filter.
-        nominally:
-            A sequence of expected lengths. The preprocessed data will be
-            trimmed to match the nearest floor of this array. Example if epath
-            data is 66 hours long it will be trimmed to 48 hours.
+            The sampling rate of the data at path.
+        downsample:
+            The downsample factor to reduce the sampling rate of the data.
+        notch_width:
+            The width of the notch filter that will be applied to the data.
+        trim_to:
+            A sequence of hours for trimming the data. The trim amount is the
+            largest trim_to amount that is below the data's actual length. E. if
+            the data is 66 hours long and trim_to is [60, 80] the data will be
+            trimmed to 60 hours duration.
         chunksize:
-            The number of processed samples along axis to yield from the 
+            The number of processed samples along axis to yield from the
             producer at a time. When called in a multiprocessing context this
             value should be set to the amount of samples any single process uses
             at one time.
         axis:
-            The axis of reading and production.
-        returning:
-            Boolean indicating if preprocessed results should be returned.
+            The axis of reading and production. For EDF files this is the last
+            axis.
         verbose:
-            Boolean indicating if timing information should be printed to
-            stdout.
+            A boolean indicating of file write progress should print to stdout.
 
     Returns:
-        None if not returning else returns preprocessed data array.
+        None
+
+    Raises:
+        A ValueError is issued of the length of the data at path is lower than
+        all the trim_to values.
     """
 
-    t0 = time.perf_counter()
+    fp = Path(path)
+    reader = edf.Reader(fp)
 
-    epath = Path(path)
-    reader = edf.Reader(epath)
-    reader.channels = channels
-
-    # Trim data with floor chosen from nominals
-    nsamples = np.array(nominally) * 3600 * fs
-    idx = np.searchsorted(nsamples, reader.shape[axis], side='right') - 1
+    # Trim data with floor chosen from trim_to
+    trim_samples = np.array(trim_to) * 3600 * fs
+    idx = np.searchsorted(trim_samples, reader.shape[axis], side='right') - 1
     if idx < 0:
         hrs = reader.shape[axis] / (3600 * fs)
-        msg = (f'File {epath.stem} is {hrs} hrs, lower than all expected'
-               f' durations {nominally} hrs.')
+        msg = f'{epath.stem} is {hrs} hrs, a value below all trim_to {trim_to}'
         raise ValueError(msg)
-    stop = nsamples[idx]
-
-    # ensure start to stop is evenly divisible by samples per record = fs/M
-    # if stop ~ k * fs then stop/M ~ k * fs / M where k is integer
+    stop = trim_samples[idx]
+    # ensure stop is integer multiple of samples per record / downsample
     stop = int(np.ceil(stop / fs) * fs)
 
     # build producer
     pro = masks.between_pro(reader, 0, stop, chunksize, axis)
-
     # Notch filter and downsample the producer
-    notch = iir.Notch(fstop, width=fwidth, fs=fs)
+    notch = iir.Notch(60, width=notch_width, fs=fs)
     result = notch(pro, chunksize, axis, dephase=False)
-    result = resampling.downsample(result, M, fs, chunksize, axis)
- 
-    # in-memory compute & write, FIXME allow writers to write pros
-    x = result.to_array()
-    print(f'downsampled shape = {x.shape}')
+    result = resampling.downsample(result, downsample, fs, chunksize, axis)
+    # FIXME openseize 1.3.0 to support write from producer
+    processed = result.to_array()
 
-    # update headers samples_per_record & num_records fields
-    header = reader.header.filter(channels)
-    print(header)
-    header['samples_per_record'] = [spr // M 
-            for spr in header['samples_per_record']]
-    header['num_records'] = int(x.shape[axis] / (fs / M))
-
-    # change the file type to EDF since we are not writing annotations
+    # prepare a header to write along with processed
+    header = copy.deepcopy(reader.header)
+    m = downsample
+    # adjust samples per record and number of records in file
+    header['samples_per_record'] = [x // m for x in header.samples_per_record]
+    header['num_records'] = int(processed.shape[axis] / (fs / m))
+    # convert to EDF from EDF+
     header['reserved_0'] = ''
-    header['names'] = ['EEG1', 'EEG2', 'EMG']
-    header['transducers'] = ['', '', '']
-    header['prefiltering'] = ['', '', '']
+    # FIXME openseize 1.3.0 to assert digital min/max always int type
     header['digital_min'] = [int(m) for m in header['digital_min']]
     header['digital_max'] = [int(m) for m in header['digital_max']]
 
-    print(header)
-
-    # write the file
-    fname = epath.stem + '_preprocessed' + epath.suffix
+    # write the standard preprocessed file
+    fname = path.stem + '_PREPROCESSED' + path.suffix
     filepath = Path(savedir).joinpath(fname)
     with edf.Writer(filepath) as writer:
-        writer.write(header, x, channels=np.arange(len(channels)), verbose=False)
+        writer.write(header, processed, reader.channels, verbose)
 
-    if verbose:
-        elapsed = time.perf_counter() - t0
-        print(f'Preprocessing & file writing finished in {elapsed} secs')
-    
-    # clean up reader resources & possibly return
     reader.close()
-    if returning:
-        return x
 
 
-def batch(dirpath, savedir, ncores=None, verbose=True, **kwargs):
-    """Preprocesses all EDF files in dir path saving the preprocessed data to
-    savedir. 
+def spindle(
+    path: Union[str, Path],
+    savedir: Union[str, Path],
+    channels: Sequence,
+    verbose: bool = True,
+) -> None:
+    """Opens an EDF file and writes a new EDF with only channels saved.
 
     Args:
+        path:
+            Path to an edf file to reduce channel count for.
+        savedir:
+            Directory where reduced channel EDF will be saved. The filename
+            will match the stem of path with the addition of '_SPINDLE' before
+            the extension.
+        channels:
+            A sequence of channel indices that will be saved to the new EDF.
+        verbose:
+            A boolean indicating if write progress should print to stdout.
+
+    Returns:
+        None
+    """
+
+    fp = Path(path)
+    reader = edf.Reader(fp)
+    data = reader.read(0)
+
+    # prepare a header to write into new EDF file with data
+    header = copy.deepcopy(reader.header)
+    # FIXME openseize 1.3.0 to assert digital min/max always int type
+    header['digital_min'] = [int(m) for m in header['digital_min']]
+    header['digital_max'] = [int(m) for m in header['digital_max']]
+
+    # write the reduced channel data
+    fname = path.stem + '_SPINDLE' + path.suffix
+    filepath = Path(savedir).joinpath(fname)
+    with edf.Writer(filepath) as writer:
+        writer.write(header, data, channels, verbose)
+
+    reader.close()
+
+
+# FIXME REFACTOR
+def batch(preprocessor, dirpath, savedir, ncores=None, verbose=True, **kwargs):
+    """Preprocesses all EDF files in dir path saving the preprocessed data to
+    savedir.
+
+    Args:
+        preprocessor:
+            A callable that preprocesses a single EDF file.
         dirpath:
             A directory containing EDF files to preprocess.
         savedir:
@@ -174,19 +193,16 @@ def batch(dirpath, savedir, ncores=None, verbose=True, **kwargs):
     Returns:
         None
     """
-    
+
     epaths = list(Path(dirpath).glob('*edf'))
-    
+
     t0 = time.perf_counter()
     if verbose:
         msg = (f'Executing Batch Preprocessor for {len(epaths)} files'
                f' in {dirpath}')
         print(msg)
-    
-    # returns are not allowed in batch processing
-    kwargs.pop('returning', False)
-    
-    func = partial(preprocess, savedir=savedir, **kwargs)
+
+    func = partial(preprocessor, savedir=savedir, **kwargs)
     workers = concurrency.set_cores(ncores, len(epaths))
     with Pool(workers) as pool:
         pool.map(func, epaths)
@@ -199,20 +215,19 @@ def batch(dirpath, savedir, ncores=None, verbose=True, **kwargs):
 
 if __name__ == '__main__':
 
-    basepath = '/media/matt/Zeus/jasmine/test/'
+    import time
+    basepath = '/media/matt/Zeus/sandy/test/'
     
-    name = 'CW0DI3_P097_KO_93_31_3dayEEG_2020-05-07_09_54_14.edf'
+    name = ('CW0DA1_P096_KO_15_53_3dayEEG'
+            '_2020-04-13_08_58_30_preprocessed.edf')
     path = Path(basepath).joinpath(name)
 
-    x = preprocess(path, savedir='/media/matt/Zeus/sandy/test/', verbose=True)
-
     """
-    savepath = '/media/matt/Zeus/sandy/test/' + '_processed' + '.edf'
-    reader = edf.Reader(savepath)
-    y = reader.read(start=0)
-
-    print(np.allclose(x, y, atol=0.5))
-
-    savedir = '/media/matt/Zeus/sandy/test/'
-    batch(basepath, savedir)
+    t0 = time.perf_counter()
+    standard(path, savedir='/media/matt/Zeus/sandy/test/', fs=5000,
+            downsample=25)
+    print(f'Elapsed {time.perf_counter() - t0} s')
     """
+
+    spindle(path, savedir='/media/matt/Zeus/sandy/test/', channels=[0,1,3])
+
